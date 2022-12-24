b86ea0e928721e6d93094f089e106490802529ef
Alexander Lavrov <Jiabp.lad@yandex.com>
2022-12-24 14:06:55 +0500

    Update: Navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,10 @@
+﻿let headerNav = document.querySelector(".header-nav");
+let navToggleButton = document.querySelector(".nav-toggle");
+let navCloseButton = document.querySelector(".nav-close");
+
+navToggleButton.addEventListener('click', toggleNavbar);
+navCloseButton.addEventListener('click', toggleNavbar);
+
+function toggleNavbar() {
+    headerNav.classList.toggle("collapse");
+}
